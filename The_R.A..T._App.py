a6bf85a8bf5990480d5ad830faa238d4cd2d70a4
Onlec <willemcallebert@gmail.com>
2026-02-12 15:12:06 +0100

    logo

--- The_R.A..T._App.py
+++ The_R.A..T._App.py
@@ -31,19 +31,7 @@ texts = all_translations.get(st.session_state.lang, all_translations["NL"]).get(
 st.title(texts.get("title", "RheoApp - TPU Rheology Expert Tool"))
 st.caption(texts.get("caption", ""))
 
-
-
-HORIZONTAL_RED = "assets/logo.png"
-ICON_RED = "assets/logo.png"
-HORIZONTAL_BLUE = "assets/logo.png"
-ICON_BLUE = "assets/logo.png"
-
-options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
-sidebar_logo = st.selectbox("Sidebar logo", options, 0)
-main_body_logo = st.selectbox("Main body logo", options, 1)
-
-st.logo(sidebar_logo, size="large", icon_image=main_body_logo)
-st.sidebar.markdown("Hi!")
+st.logo("assets/logo.png", size="large", icon_image="assets/logo.png")
 
 
 with st.expander(texts.get("disclaimer_title", "⚠️ Disclaimer")):
